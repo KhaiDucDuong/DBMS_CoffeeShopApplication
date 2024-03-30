f3d5cb474a5b3d9db8ce56c2a6819e00c473e4c2
KhaiDucDuong <greenjohnny135@gmail.com>
2024-03-31 01:25:37 +0700

    tạo trigger cập nhật số tiền tổng của OrderBill trên Insert (trước khi khấu phần bằng điểm thưởng và sau khi)

--- triggers.sql
+++ triggers.sql
@@ -1,18 +1,20 @@
-use CoffeeShop
+﻿use CoffeeShop
 GO
-/*Trigger 5*/
+/*Trigger 5 Tự tạo tài khoản cho nhân viên khi thêm một nhân viên */
 --DROP TRIGGER tr_Employee_AfterInsert
-GO
+--GO
 CREATE TRIGGER tr_Employee_AfterInsert
 ON Employee
 AFTER INSERT
 AS
 BEGIN
+	--Create accounts for employees who ARE working, accounts are ENABLED
 	INSERT INTO Account (employeeId, username, password, role, createdAt, updatedAt, isDeleted)
     SELECT employeeId, email, phoneNumber, 'Employee', CONVERT(DATETIME, GETDATE(), 103), CONVERT(DATETIME, GETDATE(), 103), 0
     FROM inserted
 	WHERE isWorking = 1
 
+	--Create accounts for employees who ARE NOT working, accounts are DISABLED
 	INSERT INTO Account (employeeId, username, password, role, createdAt, updatedAt, isDeleted)
     SELECT employeeId, email, phoneNumber, 'Employee', CONVERT(DATETIME, GETDATE(), 103), CONVERT(DATETIME, GETDATE(), 103), 1
     FROM inserted
@@ -20,9 +22,9 @@ BEGIN
 END
 
 GO
-/*Trigger 6*/
+/*Trigger 6 Khóa/Mở tài khoản của nhân viên khi cập nhật trạng thái đang làm việc của nhân viên đấy */
 --DROP TRIGGER tr_Employee_AfterUpdateIsWorking
-GO
+--GO
 CREATE TRIGGER tr_Employee_AfterUpdateIsWorking
 ON Employee
 AFTER UPDATE
@@ -48,3 +50,31 @@ BEGIN
         WHERE employeeId = @employeeId;
     END;
 END;
+
+GO
+/* Trigger 7 Cập nhật số tiền tổng của OrderBill khi thêm/sửa/xóa một sản phẩm trong OrderBillDetails */
+--DROP TRIGGER tr_OrderBill_AfterInsert
+--GO
+CREATE TRIGGER tr_OrderBill_AfterInsert
+ON OrderBillDetails
+AFTER INSERT
+AS
+BEGIN
+	--Get data to calculate the new initial bill and final bill
+	DECLARE @billId UNIQUEIDENTIFIER, @productId UNIQUEIDENTIFIER, @productPrice DECIMAL(10, 2), @productQuantity INT;
+	SELECT @billId = billId, @productId = productId, @productQuantity = quantity FROM inserted
+	SELECT @productPrice = productPrice FROM Product WHERE productId = @productId
+
+	UPDATE OrderBill
+	SET initialBill = initialBill + (@productPrice * @productQuantity),
+		finalBill = initialBill - rewardPointsUsed
+	WHERE billId = @billId
+END;
+
+/*Trigger cập nhật số điểm thưởng của khách hàng khi thêm một Order Bill - trường hợp trừ điểm thưởng vì đã sử dụng khấu phần hóa đơn*/
+
+/*Trigger cập nhật số điểm thưởng của khách hàng khi thêm một Order Bill - trường hợp cộng điểm thưởng cho hóa đơn*/
+
+/*Trigger cập nhật số điểm thưởng của khách hàng khi cập nhật finalBill của một Order Bill*/
+
+/*Trigger cập nhật số điểm thưởng của khách hàng khi xóa một Order Bill*/
