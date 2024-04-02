740f7a4cdd6dccf747e79a4139ac4e9a96074483
KhaiDucDuong <greenjohnny135@gmail.com>
2024-04-02 23:10:41 +0700

    finished delete proc for RestockBillDetails

--- procedures_Khai.sql
+++ procedures_Khai.sql
@@ -14,7 +14,7 @@ BEGIN
 END;
 
 --GO
---EXEC InsertIngredientProc N'Cà phê Legend gói 1kg', N'Trung Nguyên';
+--EXEC InsertIngredientProc N'Nguyên liệu 5', N'Trung Nguyên';
 --Go
 --select * from Ingredient
 
@@ -47,6 +47,9 @@ select * from Ingredient
 
 /* Procedure Insert vào bảng Inventory */
 GO
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'InsertInventoryProc')
+DROP PROCEDURE InsertInventoryProc
+GO
 CREATE PROCEDURE InsertInventoryProc
     @name VARCHAR(255)
 AS
@@ -55,7 +58,13 @@ BEGIN
     VALUES (@name);
 END;
 
+--GO
+--EXEC InsertInventoryProc N'Phòng 3A03';
+-- select * from Inventory
 /* Procedure Update vào bảng Inventory */
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'UpdateInventoryProc')
+DROP PROCEDURE UpdateInventoryProc
 GO
 CREATE PROCEDURE UpdateInventoryProc
     @inventoryId UNIQUEIDENTIFIER,
@@ -67,9 +76,15 @@ BEGIN
     WHERE inventoryId = @inventoryId;
 END;
 
+--GO
+--EXEC UpdateInventoryProc 'D48642D8-D205-4A1A-ACEE-A934E6A5829F', 'Phòng 2B01';
+
 /* Procedure Delete vào bảng Inventory */
 
 /* Procedure Insert vào bảng RestockBill */
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'InsertRestockBillProc')
+DROP PROCEDURE InsertRestockBillProc
 GO
 CREATE PROCEDURE InsertRestockBillProc
     @date DATE,
@@ -81,7 +96,13 @@ BEGIN
     VALUES (@date, @supplierName, @totalBill);
 END;
 
+--GO
+--EXEC InsertRestockBillProc '2/28/2024', @supplierName = 'Bách Hóa Xanh', @totalBill = '100000.00';
+
 /* Procedure Update vào bảng RestockBill */
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'UpdateRestockBillProc')
+DROP PROCEDURE UpdateRestockBillProc
 GO
 CREATE PROCEDURE UpdateRestockBillProc
     @restockBillId UNIQUEIDENTIFIER,
@@ -97,7 +118,13 @@ BEGIN
     WHERE restockBillId = @restockBillId;
 END;
 
+--GO
+--EXEC UpdateRestockBillProc 'DCEE2B02-6AA9-4F1C-8342-A87DADF34217', '2024-03-14', 'COOP', '200000.00';
+
 /* Procedure Insert vào bảng RestockBillDetails */
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'InsertRestockBillDetailsProc')
+DROP PROCEDURE InsertRestockBillDetailsProc
 GO
 CREATE PROCEDURE InsertRestockBillDetailsProc
     @ingredientId UNIQUEIDENTIFIER,
@@ -110,7 +137,12 @@ BEGIN
     VALUES (@ingredientId, @restockBillId, @quantity, @price);
 END;
 
+EXEC InsertRestockBillDetailsProc '441E3AC0-4AB2-4BEF-AF15-95FE84C44EE0', 'DCEE2B02-6AA9-4F1C-8342-A87DADF34217', '2','25000.00'; 
+
 /* Procedure Update vào bảng RestockBillDetails */
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'UpdateRestockBillDetailsProc')
+DROP PROCEDURE UpdateRestockBillDetailsProc
 GO
 CREATE PROCEDURE UpdateRestockBillDetailsProc
     @ingredientId UNIQUEIDENTIFIER,
@@ -126,7 +158,24 @@ BEGIN
     AND restockBillId = @restockBillId;
 END;
 
+--EXEC UpdateRestockBillDetailsProc '441E3AC0-4AB2-4BEF-AF15-95FE84C44EE0', 'DCEE2B02-6AA9-4F1C-8342-A87DADF34217', '4','30000.00'; 
+
 /* Procedure Delete vào bảng RestockBillDetails */
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'DeleteRestockBillDetailsProc')
+DROP PROCEDURE DeleteRestockBillDetailsProc
+GO
+CREATE PROCEDURE DeleteRestockBillDetailsProc
+    @ingredientId UNIQUEIDENTIFIER,
+    @restockBillId UNIQUEIDENTIFIER
+AS
+BEGIN
+    DELETE FROM RestockBillDetails
+    WHERE ingredientId = @ingredientId
+    AND restockBillId = @restockBillId;
+END;
+
+--EXEC DeleteRestockBillDetailsProc '441E3AC0-4AB2-4BEF-AF15-95FE84C44EE0', 'DCEE2B02-6AA9-4F1C-8342-A87DADF34217'; 
 
 
 
