d7433a1ac0b1fde9beff46c2980391d68f2fda19
ngodinhhuy-21110772 <ngodinhhuy38@gmail.com>
2024-04-02 11:31:02 +0700

    create view 3 and 4
    only create view 3 and 4

--- views.sql
+++ views.sql
@@ -13,7 +13,24 @@ join Product p on obd.productId = p.productId
 GROUP BY ob.billId, ob.rewardPointsUsed, ob.initialBill, ob.finalBill, ob.createdAt, ob.employeeId, ob.customerId
 
 --select * from GetOrderBillDetailsView where createdAt between CAST(CONVERT(VARCHAR,GETDATE(),102) AS DATETIME) and CAST(CONVERT(VARCHAR,GETDATE() + 1,102) AS DATETIME)
-
+/*Câu 3 - Xem thông tin nhân viên đang làm việc (có thể xem dựa trên role của nhân viên)*/
+GO
+IF exists(SELECT 1 FROM sys.views WHERE name='GetWorkingEmployeesView' AND type='v')
+DROP VIEW GetWorkingEmployeesView;
+GO
+CREATE VIEW GetWorkingEmployeesView AS
+SELECT *
+FROM Employee
+WHERE isWorking = 1;
+/*Câu 4 - Xem chi tiết số lượng hàng trong một đợt kiểm tra kho*/
+GO
+IF EXISTS(SELECT 1 FROM sys.views WHERE name='GetInventoryCheckDetailsView' AND type='v')
+DROP VIEW GetInventoryCheckDetailsView;
+GO
+CREATE VIEW GetInventoryCheckDetailsView AS
+SELECT icd.checkId, icd.ingredientId, i.ingredientName, icd.quantity
+FROM InventoryCheckDetails icd
+JOIN Ingredient i ON icd.ingredientId = i.ingredientId;
 /* Câu 5 - Xem các bill bán hàng trong ngày hôm nay */
 GO
 if exists(select 1 from sys.views where name='GetTodayOrderBillsView' and type='v')
